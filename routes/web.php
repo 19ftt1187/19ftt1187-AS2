f9b2e7c38584e11e6043bf98b57b198ff36b4978
ULFI SHAFFARANI <19ftt1160@student.pb.edu.bn>
2021-03-20 03:44:58 +0800

    added /as2 to underconstruction

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::group(['middleware' => 'under-construction'], function () {
+    Route::get('/as2', function () {
+        return view('welcome');
+    });
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
